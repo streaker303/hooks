723210fbd5d46e656edda4a75327d2d4b8c94fa2
streaker303 <49198429+streaker303@users.noreply.github.com>
2023-07-18 20:34:48 +0800

    useMemoizedFn

--- packages/hooks/src/useMemoizedFn/index.ts
+++ packages/hooks/src/useMemoizedFn/index.ts
@@ -20,11 +20,16 @@ function useMemoizedFn<T extends noop>(fn: T) {
 
   // why not write `fnRef.current = fn`?
   // https://github.com/alibaba/hooks/issues/728
+  
+  // fn 变化时，保证此ref持有最新的fn引用
   fnRef.current = useMemo(() => fn, [fn]);
 
   const memoizedFn = useRef<PickFunction<T>>();
+  // 保证只初始化一次
   if (!memoizedFn.current) {
+    // 构建一个函数调用的外壳，保证这个外壳函数的引用不会发生变化
     memoizedFn.current = function (this, ...args) {
+      // 调用最新函数，透传参数
       return fnRef.current.apply(this, args);
     };
   }
